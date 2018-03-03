f5b0e9e9b3fa003943197c14075d028e1ce3f967
OursDesTavernes <37019728+OursDesTavernes@users.noreply.github.com>
2018-03-03 20:17:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
         message.channel.send('tiens, voici une bonne bière! :beer:')
     	//message.reply('tiens, voici une bonne bière! :beer:');
   	}*/
-client.on('message', message => {
+client.on('message', function (user, userID, channelID, message, evt){
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
