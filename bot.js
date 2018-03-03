4902f913edcc248ca6063072f01862e35cc9e1e8
OursDesTavernes <37019728+OursDesTavernes@users.noreply.github.com>
2018-03-03 16:32:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,23 @@ client.on('message', message => {
     	message.reply('pong');
   	}
 });
-
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                client.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
