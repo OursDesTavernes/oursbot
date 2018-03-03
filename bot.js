9b5e028f05a655bed624e814962d985d4bf21350
OursDesTavernes <37019728+OursDesTavernes@users.noreply.github.com>
2018-03-03 18:59:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,29 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
+/*client.on('message', message => {
     if (message.content === '!soif') {
     	message.reply('tiens, voici une bonne bière! :beer:');
-  	}
+  	}*/
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                client.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+
 });     
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
