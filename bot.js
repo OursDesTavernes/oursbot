963b63c7b0f3cbc13379319b3766f56c1a34890f
OursDesTavernes <37019728+OursDesTavernes@users.noreply.github.com>
2018-03-04 02:46:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', message => {
     	//message.reply('tiens, voici une bonne bière! :beer:');
   	}
     else if (message.content === '!afkafé') {
-        message.reply(name,'est parti se faire un café')
+        message.reply('est parti se faire un café')
     	//message.reply('tiens, voici une bonne bière! :beer:');
   	}
     else if (message.content === '!elfes') {
