66c1808b5ab8c0163f89ef672466e491ee275baa
OursDesTavernes <37019728+OursDesTavernes@users.noreply.github.com>
2018-03-05 23:45:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,46 +5,15 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 client.on('message', message => {
-    /*if (message.content === '!soif') {
-        message.channel.send('tiens, voici une bonne bière! :beer:')
-    	//message.reply('tiens, voici une bonne bière! :beer:');
-  	}
-    else if (message.content === '!café') {
-        message.channel.send('tiens, voici un bon café! :coffee:')
-    	//message.reply('tiens, voici une bonne bière! :beer:');
-  	}
-    else if (message.content === '!super soif de la mort qui tue! TAVERNIER!') {
-        message.channel.send('TIENS,VOICI UNE PUTAIN DE TONNE DE LITRES DE BIERE!!!:beers::beers::beers::beers::beers::beers:')
-    	//message.reply('tiens, voici une bonne bière! :beer:');
-  	}
-    else if (message.content === '!afkafé') {
-        message.channel.send(message.author.toString() + ' est parti se faire un café :coffee:')
-    	//message.reply('tiens, voici une bonne bière! :beer:');
-  	}
-    else if (message.content === '!elfes') {
-        message.channel.send('les elfes sont une anomalie')
-    	//message.reply('tiens, voici une bonne bière! :beer:');
-  	}
-     else if (message.content === '!thé') {
-        message.channel.send('ON NE SERT PAS LES NEFLES ICI!')
-    	//message.reply('tiens, voici une bonne bière! :beer:');
-  	}
-    else if (message.content === '!chips') {
-        message.channel.send('enfonce toi un maïs dans le c*l et va jouer dans le micro-ondes :corn:')
-    	//message.reply('tiens, voici une bonne bière! :beer:');
-  	}
-    else if (message.content === '!talos') {
-        message.channel.send('http://i.imgur.com/c0QTX24.jpg')
-    	//message.reply('tiens, voici une bonne bière! :beer:');
-  	} 
-    else if (message.content === '!dwarf') {
-        message.channel.send('nains > elfes')
-    	//message.reply('tiens, voici une bonne bière! :beer:');
-  	}*/ 
+   
 	
-   /*client.on('guildMemberAdd', member => {
-   member.send('Bienvenue sur le serveur de Heavy-Craft');
-});*/
+   client.on('guildMemberAdd', member => {
+   if (member.role == "@everyone")//if role du membre == pas de role
+   {
+		member.send('Bienvenue sur le serveur de Heavy-Craft');
+		member.addRole("b")
+   }
+});
 	
 	switch (message.content)
 	{
