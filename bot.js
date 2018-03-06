d786d262fe512405d56a7f530f16c76afbf8992d
OursDesTavernes <37019728+OursDesTavernes@users.noreply.github.com>
2018-03-06 17:47:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,7 +86,7 @@ client.on('message', message =>
 
 		//test message carriage return  \n
 		case '!test' :
-		message.channel.send('test\n\ntest '+member.toString());
+		message.channel.send('test\n\ntest '+ member.toString());
 		break;	
 		
 		case '!testMsg' :
